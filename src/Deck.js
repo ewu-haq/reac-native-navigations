add3bb1017b49d707d6b1847cdc397a944366439
Quyen Ha <qha@humanlongevity.com>
2018-01-24 17:09:52 -0800

    add  iterpolate animation

--- src/Deck.js
+++ src/Deck.js
@@ -1,9 +1,64 @@
 import React, { Component } from "react";
-import { View, Animated } from "react-native";
+import { View, Animated, PanResponder, Dimensions } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
+const SWIPE_THRESHOLD = Dimensions.get("window").width * 0.25;
 
 class Deck extends Component {
+  constructor(props) {
+    super(props);
+
+    const position = new Animated.ValueXY();
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: (event, gesture) => {
+        if (gesture.dx > SWIPE_THRESHOLD) {
+          
+        } else if (gesture.dx < -SWIPE_THRESHOLD) {
+        } else {
+          this.resetPosition();
+        }
+      }
+    });
+
+    this.state = { panResponder, position };
+  }
+
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x: 0, y: 0 }
+    }).start();
+  }
+
+  getCardStyle() {
+    const { position } = this.state;
+    const rotate = position.x.interpolate({
+      inputRange: [-SCREEN_WIDTH * 2.0, 0, SCREEN_WIDTH * 2.0],
+      outputRange: ["-120deg", "0deg", "120deg"]
+    });
+
+    return {
+      ...this.state.position.getLayout(),
+      transform: [{ rotate }]
+    };
+  }
+
   renderCards() {
-    return this.props.data.map(item => {
+    return this.props.data.map((item, index) => {
+      if (index == 0) {
+        return (
+          <Animated.View
+            key={item.id}
+            style={this.getCardStyle()}
+            {...this.state.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      }
       return this.props.renderCard(item);
     });
   }
